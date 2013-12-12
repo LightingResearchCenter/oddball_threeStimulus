3970d55dd333611503aa7e7bb92a5e846e038608
petteriTeikari <petteri.teikari@gmail.com>
2013-12-12 13:48:21 -0500

    bug fix
    Crashes with too coarse frequency resolution

--- matlab_threeStimulus/analyze_rejectWT_underCOI.m
+++ matlab_threeStimulus/analyze_rejectWT_underCOI.m
@@ -1,18 +1,26 @@
-function power = analyze_rejectWT_underCOI(time, freq, power, coi, handles)
+function [power, nanMask] = analyze_rejectWT_underCOI(time, freq, power, coi, parameters, handles)
 
     % vectorize later
-
     rows = length(freq);
     cols = length(time);
 
     invCoi = 1 ./ coi;
-    plot(time,invCoi)
-    ylim([min(freq) max(freq)])
 
     index = zeros(length(time),1);
-    for t = 1 : cols          
-        % find the largest frequency before COI for given time point
-        index(t) = find(freq < invCoi(t), 1, 'last');
+    try
+        for t = 1 : cols          
+            % find the largest frequency before COI for given time point
+            index(t) = find(freq < invCoi(t), 1, 'last');
+        end
+    catch err
+        err
+        %invCoi(t)
+        %freq
+        find(freq < invCoi(t), 1, 'last')
+        errorStr = sprintf('%s\n%s\n%s', ['No frequencies found to reject, this means probably that you use too high temporal resolution so that the frequency resolution is bad'], ...
+                            ['default value for parameters.timeFreq.timeResolutionDivider is 16 which seem to give okay resolutin, if you go higher you get coarser time resolution'], ...
+                            ['now you are using value = ', num2str(parameters.timeFreq.timeResolutionDivider), ' -> timeRes: ', num2str(time(2)-time(1)*1000), ' ms']);
+        error(errorStr)
     end
 
     % create NaN-mask
